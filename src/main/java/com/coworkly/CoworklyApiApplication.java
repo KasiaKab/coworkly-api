3d739ac768d9fcfd4e61c653f6ba8ae3044df0fa
KasiaKab <kkate2402@gmail.com>
2025-10-09 20:04:15 +0200

    feat(app): add main Spring Boot entry point

---
+++ src/main/java/com/coworkly/CoworklyApiApplication.java
@@ -0,0 +1,12 @@
+package com.coworkly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoworklyApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoworklyApiApplication.class, args);
+    }
+
+}
