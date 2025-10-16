cf835e7dd2dc38ce610fb79b6cea4f677b0c429a
KasiaKab <kkate2402@gmail.com>
2025-10-16 18:24:26 +0200

    refactor(repo): update imports after moving entities to domain package

--- src/test/java/com/coworkly/repository/BookingRepositoryTest.java
+++ src/test/java/com/coworkly/repository/BookingRepositoryTest.java
@@ -1,7 +1,8 @@
 package com.coworkly.repository;
 
-import com.coworkly.entity.Booking;
-import com.coworkly.entity.Resource;
+import com.coworkly.domain.entity.Booking;
+import com.coworkly.domain.entity.Resource;
+import com.coworkly.domain.repository.BookingRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
