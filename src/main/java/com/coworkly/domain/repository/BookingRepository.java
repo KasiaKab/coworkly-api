f2710a1d343c8d9fc8fedf97b821beff56f2f5bf
KasiaKab <kkate2402@gmail.com>
2025-10-16 19:00:10 +0200

    feat(repo): add existsByTimeSlotId to BookingRepository

--- src/main/java/com/coworkly/domain/repository/BookingRepository.java
+++ src/main/java/com/coworkly/domain/repository/BookingRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    boolean existsByTimeSlotId(Long timeSlotId);
+
 }
