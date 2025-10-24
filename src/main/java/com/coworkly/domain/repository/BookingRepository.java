2aaecc1affdd55d062de942c15c4e949d584d864
KasiaKab <kkate2402@gmail.com>
2025-10-24 08:12:55 +0200

    feat(repo): add existsByTimeSlotIdAndStatus for active slot validation

--- src/main/java/com/coworkly/domain/repository/BookingRepository.java
+++ src/main/java/com/coworkly/domain/repository/BookingRepository.java
@@ -1,6 +1,7 @@
 package com.coworkly.domain.repository;
 
 import com.coworkly.domain.entity.Booking;
+import com.coworkly.domain.entity.BookingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +10,7 @@ import java.util.Optional;
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
-    boolean existsByTimeSlotId(Long timeSlotId);
+    boolean existsByTimeSlotIdAndStatus(Long timeSlotId, BookingStatus status);
 
     Optional<Booking> findByIdempotencyKey(String idempotencyKey);
 
