6ffeb111e9fdf51182e12e330d082e9c14a0edc8
KasiaKab <kkate2402@gmail.com>
2025-10-24 08:13:18 +0200

    refactor(exception): clarify booking-related exception messages

--- src/main/java/com/coworkly/app/exception/SlotAlreadyBookedException.java
+++ src/main/java/com/coworkly/app/exception/SlotAlreadyBookedException.java
@@ -1,5 +1,9 @@
 package com.coworkly.app.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class SlotAlreadyBookedException extends RuntimeException {
     public SlotAlreadyBookedException(Long timeSlotId) {
         super("Timeslot already booked: " + timeSlotId);
